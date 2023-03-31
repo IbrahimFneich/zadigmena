a62a3a0d4181eb34f4143bb24dea3eb36508326e
IbrahimFneich <ibrahim.fneich@hotmail.com>
2023-04-01 00:45:55 +0300

    -generate-react-cli -tailwind config -zadig transparent grey logo -Header and Main Components are in App.js

--- src/App.js
+++ src/App.js
@@ -1,77 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, FadeOut, Move, MoveIn, MoveOut, Sticky, StickyIn, StickyOut, Zoom, ZoomIn, ZoomOut } from "react-scroll-motion";
-
-const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
-const FadeUp = batch(Fade(), Move(), Sticky());
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 
 function App() {
   return (
     <div className="App">
-<ScrollContainer>
-  <ScrollPage>
-    <Animator animation={batch(Fade(), Sticky())}>
-      <span style={{ fontSize: "100px", color:"#DFE2E1"}}>Who We Are?</span>
-    </Animator>
-  </ScrollPage>
-  <ScrollPage>
-    <Animator animation={batch(Fade(), Sticky())}>
-      <span></span>
-    </Animator>
-  </ScrollPage>
-  <ScrollPage>
-    <Animator animation={batch(Fade(), Sticky())}>
-      <span style={{ fontSize: "30px", color:"#DFE2E1" }}>
-      A woman-led, privately owned company based in Lebanon. Our aim is to contribute to the development of society through empowering its members on cultural, educational, and professional levels.
-      </span>
-    </Animator>
-  </ScrollPage>
-
-  <ScrollPage>
-    <Animator animation={batch(Fade(), Sticky())}>
-      <span></span>
-    </Animator>
-  </ScrollPage>
-  <ScrollPage>
-    <Animator animation={batch(Fade(), Sticky())}>
-      <span style={{ fontSize: "100px", color:"#D2A517"}}>Our Vision</span>
-    </Animator>
-  </ScrollPage>
-  <ScrollPage>
-    <Animator animation={batch(Fade(), Sticky())}>
-      <span></span>
-    </Animator>
-  </ScrollPage>
-  <ScrollPage>
-    <Animator animation={batch(Fade(), Sticky())}>
-      <span style={{ fontSize: "30px", color:"#DFE2E1" }}>
-      We strongly believe that we are born on this earth to persevere towards creating a brighter future for people. As a company, we aspire to contribute to the development of society and empowering its members on the cultural, educational and professional levels so that our tomorrow will be better, brighter, and blooming.
-      </span>
-    </Animator>
-  </ScrollPage>
-
-  <ScrollPage>
-    <Animator animation={batch(Fade(), Sticky())}>
-      <span></span>
-    </Animator>
-  </ScrollPage>
-  <ScrollPage>
-    <Animator animation={batch(Fade(), Sticky())}>
-      <span style={{ fontSize: "100px", color:"#DFE2E1"}}>Our Mission</span>
-    </Animator>
-  </ScrollPage>
-  <ScrollPage>
-    <Animator animation={batch(Fade(), Sticky())}>
-      <span></span>
-    </Animator>
-  </ScrollPage>
-  <ScrollPage>
-    <Animator animation={batch(Fade(), Sticky())}>
-      <span style={{ fontSize: "30px", color:"#DFE2E1" }}>
-      Our goal is to support individuals and groups to lead their own journey of self-development by building and advancing their knowledge and skills; contributing to the planning and implementation of targeted projects locally, regionally, and globally; and benefiting from local and international experiences in the fields      </span>
-    </Animator>
-  </ScrollPage>
-</ScrollContainer>
+      <Header></Header>
+      <Main></Main>
     </div>
   );
 }
