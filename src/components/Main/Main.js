9e4ee777ef90bd08b9586f9c6da1532366ae69c7
IbrahimFneich <ibrahim.fneich@hotmail.com>
2023-04-09 17:18:16 +0300

    make content responsive for mobile

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -9,64 +9,73 @@ const Main = () => (
   <ScrollContainer>
     <ScrollPage>
       <Animator animation={batch(Fade(), Sticky())}>
-        <span style={{ fontSize: "100px", color:"#DFE2E1"}}>Who We Are?</span>
+        <p class="md:w-auto w-screen  md:text-8xl text-4xl text-[#DFE2E1]">
+          Who We Are?
+        </p>
       </Animator>
     </ScrollPage>
     <ScrollPage>
       <Animator animation={batch(Fade(), Sticky())}>
         <span></span>
-      </Animator>
-    </ScrollPage>
-    <ScrollPage>
-      <Animator animation={batch(Fade(), Sticky())}>
-        <span style={{ fontSize: "30px", color:"#DFE2E1" }}>
-        A woman-led, privately owned company based in Lebanon. Our aim is to contribute to the development of society through empowering its members on cultural, educational, and professional levels.
-        </span>
       </Animator>
     </ScrollPage>
 
     <ScrollPage>
       <Animator animation={batch(Fade(), Sticky())}>
-        <span></span>
-      </Animator>
-    </ScrollPage>
-    <ScrollPage>
-      <Animator animation={batch(Fade(), Sticky())}>
-        <span style={{ fontSize: "100px", color:"#D2A517"}}>Our Vision</span>
+        <p class="md:w-auto w-screen p-4 md:text-3xl text-xl text-[#DFE2E1]">
+          A woman-led, privately owned company based in Lebanon.<br/>Our aim is to contribute to the development of society through empowering its members on cultural, educational, and professional levels.
+        </p>
       </Animator>
     </ScrollPage>
     <ScrollPage>
       <Animator animation={batch(Fade(), Sticky())}>
         <span></span>
-      </Animator>
-    </ScrollPage>
-    <ScrollPage>
-      <Animator animation={batch(Fade(), Sticky())}>
-        <span style={{ fontSize: "30px", color:"#DFE2E1" }}>
-        We strongly believe that we are born on this earth to persevere towards creating a brighter future for people. As a company, we aspire to contribute to the development of society and empowering its members on the cultural, educational and professional levels so that our tomorrow will be better, brighter, and blooming.
-        </span>
       </Animator>
     </ScrollPage>
 
     <ScrollPage>
       <Animator animation={batch(Fade(), Sticky())}>
+        <p class="md:w-auto w-screen  md:text-3xl text-4xl text-[#DFE2E1]">
+          Our Vision
+        </p>
+      </Animator>
+    </ScrollPage>
+    <ScrollPage>
+      <Animator animation={batch(Fade(), Sticky())}>
+        <span></span>
+      </Animator>
+    </ScrollPage>
+
+    <ScrollPage>
+      <Animator animation={batch(Fade(), Sticky())}>
+        <p class="md:w-auto w-screen p-4 md:text-3xl text-xl text-[#DFE2E1]">
+          We strongly believe that we are born on this earth to persevere towards creating a brighter future for people.<br/>As a company, we aspire to contribute to the development of society and empowering its members on the cultural, educational and professional levels so that our tomorrow will be better, brighter, and blooming.
+        </p>
+      </Animator>
+    </ScrollPage>
+    <ScrollPage>
+      <Animator animation={batch(Fade(), Sticky())}>
+        <span></span>
+      </Animator>
+    </ScrollPage>
+
+    <ScrollPage>
+      <Animator animation={batch(Fade(), Sticky())}>
+        <p class="md:w-auto w-screen  md:text-3xl text-4xl text-[#DFE2E1]">
+          Our Mission
+        </p>
+      </Animator>
+    </ScrollPage>
+    <ScrollPage>
+      <Animator animation={batch(Fade(), Sticky())}>
         <span></span>
       </Animator>
     </ScrollPage>
     <ScrollPage>
       <Animator animation={batch(Fade(), Sticky())}>
-        <span style={{ fontSize: "100px", color:"#DFE2E1"}}>Our Mission</span>
-      </Animator>
-    </ScrollPage>
-    <ScrollPage>
-      <Animator animation={batch(Fade(), Sticky())}>
-        <span></span>
-      </Animator>
-    </ScrollPage>
-    <ScrollPage>
-      <Animator animation={batch(Fade(), Sticky())}>
-        <span style={{ fontSize: "30px", color:"#DFE2E1" }}>
-        Our goal is to support individuals and groups to lead their own journey of self-development by building and advancing their knowledge and skills; contributing to the planning and implementation of targeted projects locally, regionally, and globally; and benefiting from local and international experiences in the fields      </span>
+        <p class="md:w-auto w-screen p-4 md:text-3xl text-xl text-[#DFE2E1]">
+          Our goal is to support individuals and groups to lead their own journey of self-development by building and advancing their knowledge and skills; contributing to the planning and implementation of targeted projects locally, regionally, and globally; and benefiting from local and international experiences in the fields
+        </p>
       </Animator>
     </ScrollPage>
   </ScrollContainer>
